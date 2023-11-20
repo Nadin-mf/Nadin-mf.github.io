affbd746d6989135d4a2d66f11109d7fb835374b
Nadin-mf <146018419+Nadin-mf@users.noreply.github.com>
2023-11-20 14:03:28 +0200

    Add files via upload

---
+++ script – table/scripts/script.js
@@ -0,0 +1,30 @@
+let test = document.getElementById('myTable');
+// console.log(test)
+
+
+
+ for (let i = 0; i < test.rows.length; i++) {
+      let row = test.rows[i];
+      row.cells[i].style.backgroundColor = 'green';
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
